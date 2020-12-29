b99da44bdf4292281ceeb2a977af16a0d926c213
Rachit Gupta <65130166+rachitj1196@users.noreply.github.com>
2020-12-29 14:41:47 -0500

    Add files via upload

---
+++ 182. Duplicate Emails.sql
@@ -0,0 +1,19 @@
+/*Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+*/
+
+Select distinct a.Email from Person a
+join Person b on a.Email = b.Email and a.Id!=b.Id
